42ca2b294dcd6919487d5a5d3f8f0676467432ac
Fernando LR <fernandolr.0915@gmail.com>
2024-05-24 15:44:33 -0600

    calendarAPi tests

---
+++ tests/api/calendarApi.test.js
@@ -0,0 +1,17 @@
+import calendarApi from '../../src/api/calendarApi';
+
+describe('CalendarAPi tests', () => {
+
+    test('the base URL should be the one in the environment variables', () => {
+        expect( calendarApi.defaults.baseURL ).toBe( process.env.VITE_API_URL );
+    });
+
+    test('requests should have the x-token in the header', async() => {
+        const token = '123-abc';
+        localStorage.setItem('token', token);
+
+        const res = await calendarApi.get('/auth').then(res => res).catch(res => res);
+        expect( res.config.headers['x-token'] ).toBe( token );
+    });
+
+});
