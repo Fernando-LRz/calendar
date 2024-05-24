a41a301a1a2198b118056e53c5cf5efe53b39d74
Fernando LR <fernandolr.0915@gmail.com>
2024-05-24 13:54:43 -0600

    use different colors for user events and others

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -4,19 +4,23 @@ import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { localizer } from '../../helpers';
-import { useCalendarStore, useUiStore } from '../../hooks';
+import { useAuthStore, useCalendarStore, useUiStore } from '../../hooks';
 import { CalendarEvent, CalendarModal, FabAddNew, Navbar, FabDelete } from '../';
 
 export const CalendarPage = () => {
 
+    const { user } = useAuthStore();
     const { openDateModal } = useUiStore();
     const { events, setActiveEvent, startLoadingEvents } = useCalendarStore();
 
     const [ lastView, setLastView ] = useState( localStorage.getItem('lastView') || 'week' );
 
     const eventStyleGetter = (event, start, end, isSelected) => {
+
+        const isMyEvent = (user.uid === event.user._id) || (user.uid === event.user.uid);
+
         const style = {
-            backgroundColor: '#347CF7',
+            backgroundColor: isMyEvent ? '#347CF7' : '#465660',
             borderRadius: '0px',
             opacity: 0.8,
             color: '#fff'
